e7863762ee3c74c3e11c7352c7d70edc4b5247f7
kostya zalilov <zalilov.k@gmail.com>
2021-10-29 22:11:34 +0500

    fixed complited

--- src/components/StartContainer/StartContainer.jsx
+++ src/components/StartContainer/StartContainer.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import { StartPage } from '../../pages/StartPage/StartPage'
 import { withStartPage } from './withStartPage'
-import './StartPage.scss'
-
 
 export const StartContainer = () => {
-
   const StartComponent = withStartPage(StartPage)
-
   return <StartComponent />
 }
--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -1,17 +1,18 @@
 import React from "react"
 import { SubTitle } from "../../components/common/typografy/SubTitle"
+import './StartPage.scss'
 
 export const StartPage = ({ startInfo }) => {
   return (
     <div className="start-container">
       <SubTitle>Статьи, посвещенные Frontend-у: ReactJS, JS, ...</SubTitle>
       <div className="start-container__body">
-        {startInfo.map((start) => (
+        {startInfo.map(blog => (
           <div className="start-container__body-line">
             <div className="img-block">
-              <img src={start.img} alt="" />
+              <img src={blog.img} alt="" />
             </div>
-            <div className="text-block">{start.text}</div>
+            <div className="text-block">{blog.text}</div>
           </div>
         ))}
       </div>
