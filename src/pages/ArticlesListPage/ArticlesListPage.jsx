cd47f727eff92c278b70dc23e89e57a77a824f73
kostya zalilov <zalilov.k@gmail.com>
2021-10-29 18:35:58 +0500

    changed first article styles

--- src/pages/ArticlesListPage/ArticlesListPage.jsx
+++ src/pages/ArticlesListPage/ArticlesListPage.jsx
@@ -18,7 +18,7 @@ export const ArticlesListPage = ({ articles }) => {
           <div className="articles-list__card" key={article.id}>
             <h4>{article.title}</h4>
             <div className="article-text">
-              `{article.article.substr(0,71)}...`
+              {article.article.substr(0,71)}...
             </div>
             <button 
               className="btn btn-open-article" 
