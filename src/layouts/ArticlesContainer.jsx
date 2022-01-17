ebbc2bc45affc995f8ac204052ee095e9f12bfb4
kostya zalilov <zalilov.k@gmail.com>
2022-01-18 01:14:21 +0500

    deleted: useStore

--- src/layouts/ArticlesContainer.jsx
+++ src/layouts/ArticlesContainer.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import { ArticlesListPage } from '../pages/ArticlesListPage'
 import { ArticlePage } from '../pages/ArticlePage'
-// import { useStore } from '../hooks/useStore'
 import { useSelector } from 'react-redux'
 import { getCurrentArticle } from '../store/articles'
 
 export const ArticlesContainer = () => {
-  // const { articles, blog, handleOpenArticle } = useStore()
-  // const { blog } = useStore()
   const blog = useSelector(getCurrentArticle())
   return (
     <div className="articles">
       {blog?.length > 0 ? (
         <ArticlePage blog={blog}/>
       ) : (
-        // <ArticlesListPage articles={articles} handleOpenArticle={handleOpenArticle} />
         <ArticlesListPage />
       )
       }
