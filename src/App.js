e7b390a1843b27f47f327f608918f29ddaa5b824
kostya zalilov <zalilov.k@gmail.com>
2022-01-18 15:51:47 +0500

    short fix in App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import { useAuth } from './hooks/useAuth'
+// layouts:
 import { NavbarComponent } from './layouts/NavbarComponent'
 import { Auth } from './layouts/Auth'
-import { AdminAllPage } from './pages/AdminAllPage'
 import { ArticlesContainer } from './layouts/ArticlesContainer'
+import { FooterPage } from './layouts/FooterPage'
+// pages:
+import { AdminAllPage } from './pages/AdminAllPage'
+import { StartPage } from './pages/StartPage'
+// components:
 import { ProtectedRoute } from './components/common/ProtectedRoute'
-import { useAuth } from './hooks/useAuth'
-import './App.scss'
+import { AppLoader } from './components/ui/hoc/AppLoader'
+
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { StartPage } from './pages/StartPage'
+import './App.scss'
+// Material UI:
 import { Container } from '@material-ui/core'
-import { FooterPage } from './layouts/FooterPage'
-import { AppLoader } from './components/ui/hoc/AppLoader'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
@@ -24,7 +29,6 @@ const useStyles = makeStyles((theme) => ({
 }))
 function App () {
   const { isAuth, currentUser } = useAuth()
-  // const location = useLocation()
   const classes = useStyles()
   return (
     <>
@@ -35,7 +39,6 @@ function App () {
             <Route exact path='/' component={StartPage} />
             <Route path='/articles/:articleId?' component={ArticlesContainer} />
             <Route path='/auth/:type?' component={Auth} />
-            {/* <Route path='/admin' component={AdminContainer} /> */}
             <ProtectedRoute path='/admin' component={AdminAllPage} auth={isAuth} currentUser={currentUser} />
           </Container>
           <FooterPage />
@@ -48,5 +51,4 @@ function App () {
     </>
   )
 }
-
 export default App
