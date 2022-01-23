9a4ebf5352b1a402596717b39e0c65743417f00b
kostya zalilov <zalilov.k@gmail.com>
2022-01-24 02:34:54 +0500

    short fix

--- src/hooks/useArticles.jsx
+++ src/hooks/useArticles.jsx
@@ -1,4 +1,3 @@
-
 import React, { useContext, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getArticles, getFoundArticles, getPage, setPage } from '../store/articles'
